b0e17cbc8ef85b2f2bae3c643b8efef1f1a8368e
nidarshanshetty <nidarshanshetty97@gmail.com>
2024-02-12 15:00:12 +0530

    clean up expired tokens

--- src/main/java/com/flipcart/es/repository/RefreshTokenRepository.java
+++ src/main/java/com/flipcart/es/repository/RefreshTokenRepository.java
@@ -1,5 +1,7 @@
 package com.flipcart.es.repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,5 +12,7 @@ public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long
 {
 
 	Optional<RefreshToken> findByToken(String rt);
+
+	List<RefreshToken> findByRefreshTokenExpirationBefore(LocalDateTime now);
 
 }
--- src/main/java/com/flipcart/es/utility/ScheduledJobs.java
+++ src/main/java/com/flipcart/es/utility/ScheduledJobs.java
@@ -1,8 +1,14 @@
 package com.flipcart.es.utility;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import com.flipcart.es.entity.AccessToken;
+import com.flipcart.es.repository.AccessTokenRepository;
+import com.flipcart.es.repository.RefreshTokenRepository;
 import com.flipcart.es.repository.UserRepository;
 
 import lombok.AllArgsConstructor;
@@ -11,9 +17,10 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class ScheduledJobs
 {
-	
+
 	private UserRepository userRepository;
-	
+	private AccessTokenRepository accessTokenRepository;
+	private RefreshTokenRepository refreshTokenRepository;
 
 
 	@Scheduled(fixedDelay = 10000l)
@@ -26,8 +33,22 @@ public class ScheduledJobs
 			userRepository.delete(user);
 		});
 	}
-	
 
-	
-	
+	@Scheduled(fixedDelay = 10000l)
+	public void CleanUpExpiredTokens()
+	{
+		List<AccessToken> accessTokens = accessTokenRepository.findByAccessTokenExpirationBefore(LocalDateTime.now());
+
+		for(AccessToken accessToken:accessTokens)
+		{
+			accessTokenRepository.delete(accessToken);
+		}
+
+		refreshTokenRepository.findByRefreshTokenExpirationBefore(LocalDateTime.now())
+		.forEach(refreshToken->{
+			refreshTokenRepository.delete(refreshToken);
+		});
+	}
+
+
 }
